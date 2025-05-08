4eb76aed658b93ace4d7f4cfdf10f74c24a0b23b
Rinkal <rinkaladesara.tops@gmail.com>
2025-05-08 18:34:16 +0530

    result

--- php/max.php
+++ php/max.php
@@ -2,19 +2,31 @@
 //eLSE IF LADDER
 //Max from 2 numbers
 //Max from 3 numbers
-//Max from 4 numbers - lab task
+//Max from 4 numbers - lab task a,b,c,d using nested if & ternary operator
 if(isset($_POST['submit'])){
    $a = $_POST['a'];
    $b = $_POST['b'];
    $c = $_POST['c'];
-   if($a>$b && $a>$c){
-       echo "<br>A is max";
-   } else if($b>$c){
-        echo "<br>B is max";    
+   if($a>$b){ //a = 34 b=200 c=100
+     if($a>$c){
+          echo 'A is max';
+     } else {
+          echo 'C is max';
+     }
    } else {
-        echo "<br>C is max";
-
+          if($b>$c){
+               echo 'B is max';
+          } else {
+               echo 'C is max';
+          }
    }
+//    if($a>$b && $a>$c){
+//        echo "<br>A is max";
+//    } else if($b>$c){
+//         echo "<br>B is max";    
+//    } else {
+//         echo "<br>C is max";
+//    }
     // if($a > $b){
     //     echo "A is max";
     // } else {
---
+++ php/ternary.php
@@ -0,0 +1,10 @@
+<?php
+$a = 45;
+$b = 20;
+$c = 350;
+
+echo ($a>$b)?(($a>$c)?"A is max":"C is max"):(($b>$c)?"B is max":"C is max");
+// echo $ans;
+
+
+?>
